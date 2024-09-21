a061f6028998eb1a2a5edad513127c607bc5f261
Jasurbek-Tursunov <jasurbek3124@icloud.com>
2024-09-21 11:12:22 +0500

    integration conf to settings

--- core/settings.py
+++ core/settings.py
@@ -15,17 +15,24 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+try:
+    from conf import settings
+
+except ImportError:
+    settings = None
+
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-swy9rjousdzlsm_l&s)4(wmwz37tuuw!$+#gl8z%gsowjlcjbi'
+SECRET_KEY = getattr(settings, 'SECRET_KEY', 'django-insecure-swy9rjousdzlsm_l&s)4(wmwz37tuuw!$+#gl8z%gsowjlcjbi')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = getattr(settings, 'DEBUG', True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = getattr(settings, 'ALLOWED_HOSTS', [])
 
 
 # Application definition
